77b46639a63da36fe3b987d32aa88211c9339848
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2026-05-07 14:10:07 -0400

    Add type declarations for image file modules

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,33 @@ declare module '*.css' {
   const content: string;
   export default content;
 }
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.JPG' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
